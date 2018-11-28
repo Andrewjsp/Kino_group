14bae0bce4733820b27e5ca199bfbe7055a80f27
Berik <andre.gurkin20@mail.ru>
2018-11-28 19:43:55 +0600

    Map Collection with URI is loaded only once

--- src/main/java/serviceUri/ServiceUri.java
+++ src/main/java/serviceUri/ServiceUri.java
@@ -10,29 +10,38 @@ import java.util.Map;
 import static utill.KinoGroupConst.*;
 
 public class ServiceUri implements ActionFactory {
-public Map<String,Action> getUri(){
-    Map<String,Action>map=new HashMap<>();
-    map.put(PERSONAL_AREA,new PersonalAreaAction());
-    map.put(SHOW_ALL_GOODS,new ShowProductsForDeleteAction());
-    map.put(SHOW_INFORM_ABOUT_USER,new ShowInformForChangeRoleAction());
-    map.put(CHANGE_ROLE,new ChangeRoleAction());
-    map.put(SHOW_PRODUCTS_IN_BASKET,new ShowGoodsForBasketAction());
-    map.put(DELETE_PRODUCT,new DeleteGoodsAction());
-    map.put(SHOW_PRODUCTS,new ShowGoodsAction());
-    map.put(REGISTRATION,new RegistrationAction());
-    map.put(ADD_PRODUCTS,new ShowGoodsForAddAction());
-    map.put(ADD_PRODUCT_IN_BASKET,new AddGoodBasketAction());
-    map.put(SHOW_MUSIC,new ShowMusicAction());
-    map.put(ADD_PRODUCT,new AddGoodAction());
-    map.put(AUTORISATION,new AutorizationAction());
-    map.put(EXIT,new ExitAction());
-    map.put(SHOW_USERS,new ShowUserAction());
-    map.put(DELETE_USER,new DeleteUserAction());
-    map.put(BUY_PRODUCT,new BuyGoodAction());
-    map.put(CHANGE_LOCALE,new ChangeLocaleAction());
-    map.put(DELETE_PRODUCT_IN_BASKET,new DeleteGoodFromBasketAction());
-return map;
-}
+    private static final ServiceUri serviceUser = new ServiceUri();
+
+    private ServiceUri() {
+        getUri();
+    }
+
+    public static ServiceUri getInstance() {
+        return serviceUser;
+    }
+
+    private Map<String, Action> getUri() {
+        Map<String, Action> map = new HashMap<>();
+        map.put(SHOW_ALL_GOODS, new ShowProductsForDeleteAction());
+        map.put(SHOW_INFORM_ABOUT_USER, new ShowInformForChangeRoleAction());
+        map.put(CHANGE_ROLE, new ChangeRoleAction());
+        map.put(SHOW_PRODUCTS_IN_BASKET, new ShowGoodsForBasketAction());
+        map.put(DELETE_PRODUCT, new DeleteGoodsAction());
+        map.put(SHOW_PRODUCTS, new ShowGoodsAction());
+        map.put(REGISTRATION, new RegistrationAction());
+        map.put(SHOW_SUBPRODUCT, new ShowGoodsForAddAction());
+        map.put(ADD_PRODUCT_IN_BASKET, new AddGoodBasketAction());
+        map.put(SHOW_MUSIC, new ShowMusicAction());
+        map.put(ADD_PRODUCT, new AddGoodAction());
+        map.put(AUTORISATION, new AutorizationAction());
+        map.put(EXIT, new ExitAction());
+        map.put(SHOW_USERS, new ShowUserAction());
+        map.put(DELETE_USER, new DeleteUserAction());
+        map.put(BUY_PRODUCT, new BuyGoodAction());
+        map.put(CHANGE_LOCALE, new ChangeLocaleAction());
+        map.put(DELETE_PRODUCT_IN_BASKET, new DeleteGoodFromBasketAction());
+        return map;
+    }
 
     @Override
     public Action getAction(HttpServletRequest request) {
@@ -48,4 +57,6 @@ return map;
         }
         return action;
     }
+
+
 }
